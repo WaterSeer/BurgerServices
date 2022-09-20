f28188ac054023c7aacd86d243dc1499acb1a47c
Mike Kargin <gray_cloud@mail.ru>
2022-09-20 09:22:55 +0300

    Add CatalogAPI and BasketAPI

---
+++ src/Services/Discount/Doscount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,19 @@
+﻿using Doscount.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doscount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+
+    }
+}
